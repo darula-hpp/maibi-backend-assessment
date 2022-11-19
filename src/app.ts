5456cf0ded639c589b1b20cd6996d63e04970e3a
Olebogeng Mbedzi <olebogeng350@gmail.com>
2022-11-19 15:26:24 +0200

    update

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,1 @@
 //app
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-let listenPort  = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello Minorities");
-});
-
-app.listen(listenPort, () => {
-  console.log(`App currently running at port ${listenPort}`);
-});
